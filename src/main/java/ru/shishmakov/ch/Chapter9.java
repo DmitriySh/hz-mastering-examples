61a234de59ba7230307d8eae8a1458cb34c73419
d.shishmakov <d.shishmakov@corp.nekki.ru>
2016-09-04 19:20:17 +0300

    Were added examples for interfaces Externalizable and DataSerializable

--- src/main/java/ru/shishmakov/ch/Chapter9.java
+++ src/main/java/ru/shishmakov/ch/Chapter9.java
@@ -2,16 +2,20 @@ package ru.shishmakov.ch;
 
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializable;
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.Serializable;
+import java.io.*;
 import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
+
+import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
 
 /**
  * Created by dima on 02.09.16.
@@ -20,25 +24,42 @@ public class Chapter9 {
     private static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     public static void doExamples(HazelcastInstance hz1, HazelcastInstance hz2, ExecutorService service) {
-        serializable(hz1, hz2);
+//        useSerializition(hz1, hz2);
+//        useExternalization(hz1, hz2);
+        useDataSerializable(hz1, hz2);
     }
 
-    private static void serializable(HazelcastInstance hz1, HazelcastInstance hz2) {
-        logger.debug("-- HZ serializable --");
+    private static void useDataSerializable(HazelcastInstance hz1, HazelcastInstance hz2) {
+        logger.debug("-- HZ DataSerializable --");
 
-        IMap<PersonSerial, Integer> mapBin = hz1.getMap("mapBin");
-        IMap<PersonSerial, Integer> mapObj = hz1.getMap("mapObj");
+        processKeyMap(hz1, hz2, new PersonDataSerial("Dmitriy", "Shishmakov", "History"));
+    }
 
-        PersonSerial person = new PersonSerial("Dima", "Shishmakov", "History");
+    private static void useExternalization(HazelcastInstance hz1, HazelcastInstance hz2) {
+        logger.debug("-- HZ Externalizable --");
+
+        processKeyMap(hz1, hz2, new PersonExternal("Dmitriy", "Shishmakov", "History"));
+    }
+
+    private static void useSerializition(HazelcastInstance hz1, HazelcastInstance hz2) {
+        logger.debug("-- HZ Serializable --");
+
+        processKeyMap(hz1, hz2, new PersonSerial("Dmitriy", "Alexandrovich", "Shishmakov", "History"));
+    }
+
+    private static void processKeyMap(HazelcastInstance hz1, HazelcastInstance hz2, Person person) {
+        IMap<Person, Integer> mapBin = hz1.getMap("mapBin");
+        IMap<Person, Integer> mapObj = hz1.getMap("mapObj");
+
         logger.debug("original key: {}", person);
 
-        mapBin.set(person, 1);
-        mapObj.set(person, 1);
+        mapBin.set(person, 2);
+        mapObj.set(person, 2);
         logger.debug("mapBin contains key: {}", mapBin.containsKey(person));
         logger.debug("mapObj contains key: {}", mapObj.containsKey(person));
 
-        PersonSerial[] mapBinKeys = mapBin.keySet().toArray(new PersonSerial[0]);
-        PersonSerial[] mapObjKeys = mapObj.keySet().toArray(new PersonSerial[0]);
+        Person[] mapBinKeys = mapBin.keySet().toArray(new Person[0]);
+        Person[] mapObjKeys = mapObj.keySet().toArray(new Person[0]);
         logger.debug("mapBin keys: {}", Arrays.toString(mapBinKeys));
         logger.debug("mapObj keys: {}", Arrays.toString(mapObjKeys));
 
@@ -49,18 +70,132 @@ public class Chapter9 {
         mapObj.destroy();
     }
 
-    public static class PersonSerial implements Serializable {
+    public static class PersonDataSerial extends Person implements DataSerializable {
+
+        public PersonDataSerial() {
+            super();
+            /* need to be */
+        }
+
+        public PersonDataSerial(String name, String surname, String hobby) {
+            super(name, surname, hobby);
+        }
+
+        @Override
+        public void writeData(ObjectDataOutput out) throws IOException {
+            out.writeUTF(name);
+
+        }
+
+        @Override
+        public void readData(ObjectDataInput in) throws IOException {
+            this.name = in.readUTF();
+
+        }
+    }
+
+
+    public static class PersonExternal extends Person implements Externalizable {
+
+        public PersonExternal() {
+            super();
+            /* need to be */
+        }
+
+        public PersonExternal(String name, String surname, String hobby) {
+            super(name, surname, hobby);
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeUTF(name);
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            this.name = in.readUTF();
+        }
+    }
+
+    public static class PersonSerial extends Person implements Serializable {
         private static final long serialVersionUID = 1L;
 
-        private /* not final */ String name;
-        private /* not final */ transient String surname;
-        private /* not final */ transient String hobby = "rugby";
-        private static /* not final */ String password = "password123";
+        private String secondName; // only this field will be deserialized
 
-        public PersonSerial(String name, String surname, String hobby) {
+        public PersonSerial(String name, String secondName, String surname, String hobby) {
+            super(name, surname, hobby);
+            this.secondName = secondName;
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this, SHORT_PREFIX_STYLE)
+                    .append("name", name)
+                    .append("secondName", secondName)
+                    .append("surname", surname)
+                    .append("hobby", hobby)
+                    .append("password", password)
+                    .toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || !(o instanceof PersonSerial)) return false;
+            if (!super.equals(o)) return false;
+            PersonSerial that = (PersonSerial) o;
+            return Objects.equals(secondName, that.secondName) &&
+                    Objects.equals(name, that.name) &&
+                    Objects.equals(surname, that.surname) &&
+                    Objects.equals(hobby, that.hobby);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(super.hashCode(), secondName);
+        }
+    }
+
+
+    public static class Person {
+        protected /* not final */ String name;
+        protected /* not final */ transient String surname;
+        protected /* not final */ transient String hobby = "rugby";
+        protected static /* not final */ String password = "password123";
+
+        Person() {
+            /* need to be */
+        }
+
+        protected Person(String name, String surname, String hobby) {
             this.name = name;
             this.surname = surname;
             this.hobby = hobby;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || !(o instanceof Person)) return false;
+            Person that = (Person) o;
+            return Objects.equals(name, that.name) &&
+                    Objects.equals(surname, that.surname) &&
+                    Objects.equals(hobby, that.hobby);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, surname, hobby);
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this, SHORT_PREFIX_STYLE)
+                    .append("name", name)
+                    .append("surname", surname)
+                    .append("hobby", hobby)
+                    .append("password", password)
+                    .toString();
         }
 
         public String getName() {
@@ -77,31 +212,6 @@ public class Chapter9 {
 
         public static String getPassword() {
             return password;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || !(o instanceof PersonSerial)) return false;
-            PersonSerial that = (PersonSerial) o;
-            return Objects.equals(name, that.name) &&
-                    Objects.equals(surname, that.surname) &&
-                    Objects.equals(hobby, that.hobby);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, surname, hobby);
-        }
-
-        @Override
-        public String toString() {
-            return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
-                    .append("name", name)
-                    .append("surname", surname)
-                    .append("hobby", hobby)
-                    .append("password", password)
-                    .toString();
         }
     }
 }
